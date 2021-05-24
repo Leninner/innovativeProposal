fd234a790f554ca4cf4effbc787265fb4d03e6c8
Leninner <Mazabandalenin180@gmail.com>
2021-05-24 18:20:48 -0500

    Añado diseño a los botones y algunas imágenes

--- juice.js
+++ juice.js
@@ -1,8 +1,3 @@
 // const visitante = prompt("Cuál es tu nombre?");
 
 // alert("Hola, " + visitante + " es un gusto saludarte!");
-
-const btn = document.querySelector("#btnForm");
-btn.addEventListener("click", () => {
-    alert("Gracias por tu tiempo! \nSerá un gusto atender tu llamado");
-});
