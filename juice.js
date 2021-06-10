ab90a7c84784b1245a1939ea1477323840b3a8c4
Leninner <Mazabandalenin180@gmail.com>
2021-06-10 09:03:14 -0500

    Añado saludo para compañeros de equipo

--- juice.js
+++ juice.js
@@ -2,9 +2,17 @@ const visitante = prompt("Cuál es tu nombre?");
 saludo(visitante);
 
 function saludo(nombre) {
-    alert(
-        "Hola, " +
-            nombre.trim().replace(/^\w/, (c) => c.toUpperCase()) +
-            " es un gusto saludarte!"
-    );
+    if (
+        nombre.trim().replace(/^\w/, (c) => c.toUpperCase()) == "Luis" ||
+        nombre.trim().replace(/^\w/, (c) => c.toUpperCase()) == "Erick" ||
+        nombre.trim().replace(/^\w/, (c) => c.toUpperCase()) == "Bryan"
+    ) {
+        alert("Hola manquito");
+    } else {
+        alert(
+            "Hola, " +
+                nombre.trim().replace(/^\w/, (c) => c.toUpperCase()) +
+                " es un gusto saludarte!"
+        );
+    }
 }
